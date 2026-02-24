8bdb334939f3ef3d684e83a046fe930e286a7365
cfranklin121 <courtenayfranklin@gmail.com>
2026-02-23 23:11:34 -0700

    Updated REPL logic and added commands

--- commands.go
+++ commands.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
-}
-
-func commandExit() error {
-	fmt.Println("Closing Pokedex...Goodbye!")
-	os.Exit(0)
-	return fmt.Errorf("")
 }
--- main.go
+++ main.go
@@ -12,16 +12,43 @@ func main() {
 	commands := map[string]cliCommand{
 		"exit": {
 			name:        "exit",
-			description: "Exits the Pokedex",
-			callback:    commandExit(),
+			description: "Exits the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
 		},
 	}
+
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		text := scanner.Text()
-		words := cleanInput(text)
-		fmt.Println("Your command was:", words[0])
+		input := cleanInput(text)
 
+		for key := range commands {
+			if input[0] == key {
+				fmt.Println(commands[input[0]].description)
+				err := commands[input[0]].callback()
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				fmt.Println("Unknown command")
+			}
+		}
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing Pokedex...Goodbye!")
+	os.Exit(0)
+	return fmt.Errorf("")
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:")
+	return nil
 }
