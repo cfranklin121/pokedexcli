09759c3bf2fefb2a622a9cb0de82830abed43217
cfranklin121 <courtenayfranklin@gmail.com>
2026-03-09 20:13:49 -0700

    Placeholders for Add, Get, and NewCache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -15,6 +15,25 @@ type cacheEntry struct {
 	val       []byte
 }
 
-func (c *Cache) Add(key string) ([]byte, bool)
+func (c *Cache) Add(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	//Add code
+	return []byte{}, false
+}
 
-func (c *Cache) Get(key string) ([]byte, bool)
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	//Add code
+	return []byte{}, false
+}
+
+/*
+func NewCache(interval int) Cache {
+	new := Cache{
+		cache: make(map[string]cacheEntry),
+		mu: m,
+	}
+}
+*/
