2961011792918097e035b5be36587a23e587bccc
cfranklin121 <courtenayfranklin@gmail.com>
2026-03-13 13:05:21 -0700

    Commenting out cache functionality until later

--- commandMap.go
+++ commandMap.go
@@ -8,21 +8,22 @@ import (
 )
 
 func commandMap(config *Config) error {
-	body, ok := config.cache.Get(config.location.Next) //Get response from cache
-	if ok {
-		err := json.Unmarshal(body, &config.location)
-		if err != nil {
-			return fmt.Errorf("Error: %s", err)
+	/*
+		body, ok := config.cache.Get(config.location.Next) //Get response from cache
+		if ok {
+			err := json.Unmarshal(body, &config.location)
+			if err != nil {
+				return fmt.Errorf("Error: %s", err)
 
+			}
+			results := config.location.Results
+			for _, r := range results {
+				fmt.Println(r.Name)
+			}
+			config.cache.Add(config.location.Next, body) //Add response to cache
+			return nil
 		}
-		results := config.location.Results
-		for _, r := range results {
-			fmt.Println(r.Name)
-		}
-
-		return nil
-	}
-
+	*/
 	if len(config.location.Results) == 0 {
 		res, err := http.Get("https://pokeapi.co/api/v2/location-area/")
 		if err != nil {
