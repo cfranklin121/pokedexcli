ef9febaf15460e281c394df9d585c97931aaa543
cfranklin121 <courtenayfranklin@gmail.com>
2026-03-14 13:51:06 -0700

    Catch basics, pokemon struct

--- commandCatch.go
+++ commandCatch.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/cfranklin121/pokedexcli/internal/pokeapi"
+)
 
 func commandCatch(config *Config) error {
-	return fmt.Errorf("Not implemented yet\n")
+	baseUrl := "https://pokeapi.co/api/v2/pokemon/"
+	if config.commandArg == "" {
+		return fmt.Errorf("Please enter pokemon name")
+	}
+
+	data, err := pokeapi.GetApiData(baseUrl + config.commandArg)
+	if err != nil {
+		return err
+	}
+
+	var pokemon Pokemon
+	err = json.Unmarshal(data, &pokemon)
+	if err != nil {
+		return fmt.Errorf("Error: %s", err)
+	}
+
+	fmt.Println(pokemon.Name)
+	fmt.Println(pokemon.BaseExperience)
+	return nil
 }
